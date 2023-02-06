934822dfca33e4085c47ea980c1b0d2440e3f23f
Oleh Hrubnik <55631021+Suprelf@users.noreply.github.com>
2023-02-06 10:26:05 +0200

    Update app.module.ts
    cleaning up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -23,7 +22,6 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     FormsModule,
 
     MatSelectModule,
